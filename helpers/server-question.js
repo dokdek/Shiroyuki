14cdea2811c5495ced8fa2f39268687e673f4553
dokdek <dokdek@gmail.com>
2021-01-18 20:34:35 +0000

    Completed skeleton for creation with no options

--- helpers/server-question.js
+++ helpers/server-question.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
-const sizePlay = require("./size-play");
+const serverSave = require("./server-save");
+
+//Make it into a promise chain instead of doing whatever this is.
 
 const serverQuestion = (user, datacenter) => {
   let serverList;
@@ -26,44 +28,146 @@ const serverQuestion = (user, datacenter) => {
               user.id !== msg.author.id
             ); //filter for only the emojis the user reacted with.
           };
-          msg.awaitReactions(filter, { max: 1 }).then((collected) => {
-            const userReaction = collected.last();
-            //extract this out to helper to reduce code clutter, too many servers.
-            let server;
-            if (userReaction.emoji.name === "🇬") {
-              server = "Gilgamesh";
-            }
-
-            msg.delete();
-            embeddedMessage
+          msg
+            .awaitReactions(filter, { max: 1 })
+            .then((collected) => {
+              const userReaction = collected.last();
+              //extract this out to helper to reduce code clutter, too many servers.
+              let server;
+              if (userReaction.emoji.name === "🇬") {
+                server = "Gilgamesh";
+              }
+              msg.delete();
+              embeddedMessage
                 .setTitle("What is the size of your FC?")
                 .setDescription("Small: < 20 \n Medium: < 50 \n Large: > 100");
-            user
-              .send(embeddedMessage)
-              .then((msg) => {
-                Promise.all([msg.react("🇸"), msg.react("🇲"), msg.react("🇱")])
-                  .then(() => {
-                    const filter = (reaction, user) => {
-                      return (
-                        ["🇸", "🇲", "🇱"].includes(reaction.emoji.name) &&
-                        user.id !== msg.author.id
-                      ); //filter for only the emojis the user reacted with.
-                    };
-                    msg.awaitReactions(filter, { max: 1 }).then((collected) => {
-                      const userReaction = collected.last();
-                      //Need to add other sizes
-                      let size;
-                      if (userReaction.emoji.name === "🇸") {
-                        size = "Small";
-                      }
-                      msg.delete();
-                      user.send("playstyle placeholder");
-                    });
-                  })
-                  .catch((err) => console.log(err));
-              })
-              .catch((err) => console.log(err));
-          });
+              user
+                .send(embeddedMessage)
+                .then((msg) => {
+                  let name;
+                  let description;
+                  Promise.all([
+                    msg.react("🇸"),
+                    msg.react("🇲"),
+                    msg.react("🇱"),
+                  ])
+                    .then(() => {
+                      const filter = (reaction, user) => {
+                        return (
+                          ["🇸", "🇲", "🇱"].includes(reaction.emoji.name) &&
+                          user.id !== msg.author.id
+                        ); //filter for only the emojis the user reacted with.
+                      };
+                      msg
+                        .awaitReactions(filter, { max: 1 })
+                        .then((collected) => {
+                          const userReaction = collected.last();
+                          //Need to add other sizes
+                          let size;
+                          if (userReaction.emoji.name === "🇸") {
+                            size = "Small";
+                          }
+                          msg.delete();
+                          embeddedMessage
+                            .setTitle("What is the playstyle of your FC?")
+                            .setDescription(
+                              "🇨asual\n🇲idcore\n🇭ardcore/competitive"
+                            );
+                          user
+                            .send(embeddedMessage)
+                            .then((msg) => {
+                              Promise.all([
+                                msg.react("🇨"),
+                                msg.react("🇲"),
+                                msg.react("🇭"),
+                              ])
+                                .then(() => {
+                                  const filter = (reaction, user) => {
+                                    return (
+                                      ["🇨", "🇲", "🇭"].includes(
+                                        reaction.emoji.name
+                                      ) && user.id !== msg.author.id
+                                    ); //filter for only the emojis the user reacted with.
+                                  };
+                                  msg
+                                    .awaitReactions(filter, { max: 1 })
+                                    .then((collected) => {
+                                      const userReaction = collected.last();
+                                      //Need to add other playstyles
+                                      let playstyle;
+                                      if (userReaction.emoji.name === "🇨") {
+                                        playstyle = "Casual";
+                                      }
+                                      msg.delete();
+                                      embeddedMessage
+                                        .setTitle(
+                                          "What is the name of your FC?"
+                                        )
+                                        .setDescription("Please type below");
+                                      user
+                                        .send(embeddedMessage)
+                                        .then((msg) => {
+                                          const dmFilter = (m) =>
+                                            m.content.length > 0 &&
+                                            user.id !== msg.author.id;
+                                          user.dmChannel
+                                            .awaitMessages(dmFilter, { max: 1 })
+                                            .then((collected) => {
+                                              name = collected.first();
+                                              embeddedMessage
+                                                .setTitle(
+                                                  "Please list a description"
+                                                )
+                                                .setDescription(
+                                                  "Please type below"
+                                                );
+                                              user
+                                                .send(embeddedMessage)
+                                                .then((msg) => {
+                                                  const dmFilter = (m) =>
+                                                    m.content.length > 0 &&
+                                                    user.id !== msg.author.id;
+                                                  user.dmChannel
+                                                    .awaitMessages(dmFilter, {
+                                                      max: 1,
+                                                    })
+                                                    .then((collected) => {
+                                                      description = collected.first();
+                                                      serverSave(
+                                                        datacenter,
+                                                        server,
+                                                        name,
+                                                        description,
+                                                        size,
+                                                        playstyle,
+                                                        user
+                                                      );
+                                                    })
+                                                    .catch((err) =>
+                                                      console.log(err)
+                                                    );
+                                                })
+                                                .catch((err) =>
+                                                  console.log(err)
+                                                );
+                                            })
+                                            .catch((err) => console.log(err));
+                                        })
+                                        .catch((err) => console.log(err));
+                                    })
+                                    .catch((err) => console.log(err));
+                                })
+                                .catch((err) => console.log(err));
+                            })
+                            .catch((err) => console.log(err));
+                        })
+                        .catch((err) => console.log(err));
+                    })
+                    .catch((err) => console.log(err));
+                })
+                .catch((err) => console.log(err));
+            })
+            .catch((err) => console.log(err));
         })
         .catch((err) => console.log(err));
     })
