d6c595ce230caa2095492a618d18dcdb4926537b
dokdek <dokdek@gmail.com>
2021-05-03 16:27:11 -0400

    Updated minimum score for random post

--- helpers/randomHourly.js
+++ helpers/randomHourly.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const randomHourly = (message) =>{
     const channel = message.channel;
     setInterval(()=>{
-        const requestUrl = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=1&tags=breasts~ sort:random score:>=5 -censored -uncensored -rape -torture -nude -pussy -shota {hololive ~ azur_lane ~ umamusume ~ arknights ~ princess_connect!_re%3Adive ~ princess_connect!}";
+        const requestUrl = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=1&tags=breasts~ sort:random score:>=20 -censored -uncensored -rape -torture -nude -pussy -shota {hololive ~ azur_lane ~ umamusume ~ arknights ~ princess_connect!_re%3Adive ~ princess_connect!}";
         axios.get(requestUrl)
             .then((res)=>{
                 channel.send(res.data[0].file_url);
