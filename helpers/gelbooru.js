839e537a5fd15b2ad9acf438f549e36c535f1079
dokdek <dokdek@gmail.com>
2021-05-03 21:44:26 -0400

    Change source to title in embed

--- helpers/gelbooru.js
+++ helpers/gelbooru.js
@@ -6,6 +6,7 @@ const gelbooru = (message, args) =>{
     const requestUrl = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=1&tags=breasts~ sort:random score:>=10 -censored -uncensored -rape -torture -nude -pussy -shota ";
 
     const embeddedMessage = new Discord.MessageEmbed()
+    .setFooter("Gelbooru", "https://pbs.twimg.com/profile_images/1118350008003301381/3gG6lQMl_400x400.png")
     .setColor("#DBFFFF");
 
     axios.get(requestUrl.concat(args))
@@ -19,7 +20,8 @@ const gelbooru = (message, args) =>{
                         if(res.data.length > 0){
                             embeddedMessage.setDescription("Could not find any results with your exact tag, looking for an image with a similar tag.")
                             .setImage(res.data[0].file_url)
-                            .setAuthor("Click me for source",'',res.data[0].source);
+                            .setTitle("Click me for source")
+                            .setURL(res.data[0].source);
                             message.channel.send(embeddedMessage);
                         }else{
                             embeddedMessage.setDescription("Could not find any results with your exact tag or similar tags.");
@@ -33,7 +35,8 @@ const gelbooru = (message, args) =>{
                     })
             }else{
                 embeddedMessage.setImage(res.data[0].file_url)
-                    .setAuthor("Click me for source",'',res.data[0].source);
+                .setTitle("Click me for source")
+                .setURL(res.data[0].source);
                 message.channel.send(embeddedMessage);
             }
         })
